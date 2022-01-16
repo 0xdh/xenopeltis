85800589e5c74d2a2686853718374a5b0bca0719
Daniel He <he.daniel@protonmail.com>
2022-01-16 12:57:44 +0100

    Implements shared types

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,8 +1,69 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use serde::{Serialize, Deserialize};
+
+/// Messages coming from the client to the server.
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum ClientMessage {
+    Direction(DirectionMessage),
+    Quit,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+/// Client pressed a direction key, lets server know
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct DirectionMessage {
+    pub direction: Direction,
+}
+
+/// Messages coming from the server to the client.
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum ServerMessage {
+    FieldChange(FieldChangeMessage),
+    GameState(GameStateMessage),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum GameStateMessage {
+    Playing,
+    Won,
+    Lost,
+}
+
+/// RGB color.
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
+
+/// What is in a field?
+///
+/// Can be empty, apple (edible) or snake. Snakes are differentiated
+/// by their color.
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Field {
+    Empty,
+    Apple,
+    Snake(Color),
+}
+
+/// Represents a coordinate on the game field
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Coordinate {
+    pub x: usize,
+    pub y: usize,
+}
+
+/// A field has just changed the field type
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FieldChangeMessage {
+    pub coordinate: Coordinate,
+    pub field: Field,
 }
